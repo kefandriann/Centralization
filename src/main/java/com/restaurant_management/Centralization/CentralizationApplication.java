36d0cf195764e6c40a9f19a610bf28b8311edcc5
Kef <hei.kephas@gmail.com>
2025-04-18 15:27:27 +0300

    Initial commit

---
+++ src/main/java/com/restaurant_management/Centralization/CentralizationApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant_management.Centralization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizationApplication.class, args);
+	}
+
+}
